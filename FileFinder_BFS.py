f38323d533bf320a50441d10ecb5831b5f2dbd04
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2016-12-10 19:15:40 +0530

    Update FileFinder_BFS.py

--- FileFinder_BFS.py
+++ FileFinder_BFS.py
@@ -1,61 +1,52 @@
 import os
 import sys
 import time
+ispy3 = sys.version[0] == '3'
 
-#import Queue acc to pyhton version
 
-def main():
-	start = time.time()
-
-	ispy3 = sys.version[0] == '3'
-
-	if ispy3:
-	    import queue as queue
+def DFS( root,tr,ind,level):
+	#print("\033[1;32;40m",ind+'>'+root)
+	try :
+		file = os.listdir(root)
+	except :
+		print("We Don't have permission to access : ",root)
 	else:
-	    import Queue as queue
-
-	#root = '/home/akgarhwal/LPU'
-	root = os.getcwd()
-	print("Default Root Folder : ",root,"\nEnter root folder path[Enter for Skip] : ")
-	temp = input()
-
-	if(temp.__len__()>0):
-		root=temp
-	path=''
-	q=queue.Queue(maxsize=0)
-	q.put(root)
-
-	print("Enter Name/Extension of File : ")
-	ext =''
-	if ispy3:
-		ext = input()
-	else:
-		ext = raw_input()
-	LEN = ext.__len__()
-
-	print("START SCANNING...\n")
-
-	while((q.empty())==False):
-		path = q.get()
-		#print(path)
-		file = os.listdir(path)
 		len = (file.__len__())
 		for i in range(0,len):
 
 			if(file[i][0]!='.'):
-				Entry =path+'/'+file[i]
-
-				if(file[i].find(ext)!=-1):
-						print(Entry)
+				Entry =root+'/'+file[i]
 
 				if((os.path.isdir(Entry))):
-					#print('  '+file[i])
-					q.put(Entry)
-				#else:
-				#	if(file[i].find(ext)!=-1):
-				#		print('\t\t'+file[i])
+					if(file[i].find(ext)!=-1):
+						print("\033[0;45;40m",level,tr+'\t'+file[i])
+					DFS(Entry,(tr+'\t'),ind+'--------',level+1)
+				else:
+					if(file[i].find(ext)!=-1):
+						print("\033[0;45;40m",level,tr+'\t'+file[i])
+						#print(level,tr+'\t'+file[i])
 
-	print("FINISHED.\nTotal Time : ",time.time()-start)
+root = os.getcwd()
+print("Default Root Folder : ",root,"\nEnter root folder path[Enter for Skip] : ")
+if ispy3:
+	temp = input()
+else:
+	temp = raw_input()
 
+if(temp.__len__()>0):
+	root=temp
 
-main()
+print("Enter Name/Extension of File : ")
+ext =''
+if ispy3:
+	ext = input()
+else:
+	ext = raw_input()
+
+start = time.time()
+ind=''
+print("Start Scanning....\n")
+
+DFS(root,'',ind,0)
+
+print("FINISHED.\nTotal Time : ",time.time()-start)
