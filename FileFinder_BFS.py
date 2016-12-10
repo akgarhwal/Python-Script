ea280e9128cc6c15304026a556feab6045c622ce
Abhinesh Garhwal <abhinesh.garhwal@gmail.com>
2016-12-10 19:16:54 +0530

    Update FileFinder_BFS.py

--- FileFinder_BFS.py
+++ FileFinder_BFS.py
@@ -1,52 +1,70 @@
+#Search for hidden file also
+#Using BFS
+
 import os
 import sys
 import time
-ispy3 = sys.version[0] == '3'
 
+#import Queue acc to pyhton version
 
-def DFS( root,tr,ind,level):
-	#print("\033[1;32;40m",ind+'>'+root)
-	try :
-		file = os.listdir(root)
-	except :
-		print("We Don't have permission to access : ",root)
+def main():
+	start = time.time()
+
+	ispy3 = sys.version[0] == '3'
+
+	if ispy3:
+	    import queue as queue
 	else:
-		len = (file.__len__())
-		for i in range(0,len):
+	    import Queue as queue
 
-			if(file[i][0]!='.'):
-				Entry =root+'/'+file[i]
+	#root = '/home/akgarhwal/LPU'
+	root = os.getcwd()
+	print("Default Root Folder : ",root,"\nEnter root folder path[Enter for Skip] : ")
+	if ispy3:
+		temp = input()
+	else:
+		temp = raw_input()
+
+	if(temp.__len__()>0):
+		root=temp
+	path=''
+	q=queue.Queue(maxsize=0)
+	q.put(root)
+
+	print("Enter Name/Extension of File : ")
+	ext =''
+	if ispy3:
+		ext = input()
+	else:
+		ext = raw_input()
+	LEN = ext.__len__()
+
+	print("START SCANNING...\n")
+
+	while((q.empty())==False):
+		path = q.get()
+		#print(path)
+		try :
+			file = os.listdir(path)
+		except :
+			print("We Don't have permission to access : ",path)
+		else:
+			len = (file.__len__())
+			for i in range(0,len):
+
+				Entry =path+'/'+file[i]
+
+				if(file[i].find(ext)!=-1):
+						print(Entry)
 
 				if((os.path.isdir(Entry))):
-					if(file[i].find(ext)!=-1):
-						print("\033[0;45;40m",level,tr+'\t'+file[i])
-					DFS(Entry,(tr+'\t'),ind+'--------',level+1)
-				else:
-					if(file[i].find(ext)!=-1):
-						print("\033[0;45;40m",level,tr+'\t'+file[i])
-						#print(level,tr+'\t'+file[i])
+					#print('  '+file[i])
+					q.put(Entry)
+				#else:
+				#	if(file[i].find(ext)!=-1):
+				#		print('\t\t'+file[i])
 
-root = os.getcwd()
-print("Default Root Folder : ",root,"\nEnter root folder path[Enter for Skip] : ")
-if ispy3:
-	temp = input()
-else:
-	temp = raw_input()
+	print("FINISHED.\nTotal Time : ",time.time()-start)
 
-if(temp.__len__()>0):
-	root=temp
 
-print("Enter Name/Extension of File : ")
-ext =''
-if ispy3:
-	ext = input()
-else:
-	ext = raw_input()
-
-start = time.time()
-ind=''
-print("Start Scanning....\n")
-
-DFS(root,'',ind,0)
-
-print("FINISHED.\nTotal Time : ",time.time()-start)
+main()
